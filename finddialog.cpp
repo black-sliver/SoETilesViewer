0bd0500047adf57a56ec8cb705d1aa150a1a401e
black-sliver <>
2020-08-28 22:45:31 +0200

    fix find dialog focus

--- finddialog.cpp
+++ finddialog.cpp
@@ -3,17 +3,19 @@
 #include <QPushButton>
 
 FindDialog::FindDialog(QWidget *parent, const QString& text, QTextDocument::FindFlags flags, bool regex) :
-    QDialog(parent),
+    QDialog(parent, Qt::WindowSystemMenuHint | Qt::WindowTitleHint),
     ui(new Ui::FindDialog)
 {
     ui->setupUi(this);
+    setFixedSize(width(), height()); // hide maximize button on linux
     ui->buttonBox->button(QDialogButtonBox::Ok)->setText("Find");
     ui->lineEdit->setText(text);
     ui->lineEdit->setSelection(0, ui->lineEdit->text().length());
     ui->chkRegex->setChecked(regex);
-    this->_flags = flags;
+    _flags = flags;
     ui->chkCaseSensitive->setChecked(flags & QTextDocument::FindCaseSensitively);
-    setWindowFlag(Qt::Tool);
+
+    ui->lineEdit->setFocus();
 }
 
 FindDialog::~FindDialog()
